411d7a86f950d99ce017b9cf37cf082a348adf59
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:49:03 +0530

    improve

--- frontend/test/json_code_snippet_view_test.dart
+++ frontend/test/json_code_snippet_view_test.dart
@@ -3,12 +3,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const jsonString = '{"key": "value"}';
+  const String jsonString = '{"key": "value"}';
 
-  testWidgets('Renders JsonCodeSnippetView without crashing',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(
-        const MaterialApp(home: JsonCodeSnippetView(jsonString: jsonString)));
-    expect(find.byType(JsonCodeSnippetView), findsOneWidget);
+  group('JsonCodeSnippetView tests:', () {
+    testWidgets('renders without crashing', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: JsonCodeSnippetView(jsonString: jsonString),
+          ),
+        ),
+      );
+
+      expect(find.byType(JsonCodeSnippetView), findsOneWidget);
+    });
+
+    testWidgets('displays the JSON string correctly', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: JsonCodeSnippetView(jsonString: jsonString),
+          ),
+        ),
+      );
+
+      expect(find.text('"key": "value"'), findsOneWidget);
+    });
   });
 }
