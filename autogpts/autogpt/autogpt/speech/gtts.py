364f6398a0bcea1b6eae7b390cc04c7f988ad937
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:45:39 +0530

    improve

--- autogpts/autogpt/autogpt/speech/gtts.py
+++ autogpts/autogpt/autogpt/speech/gtts.py
@@ -2,23 +2,29 @@
 from __future__ import annotations
 
 import os
-
-import gtts
-from playsound import playsound
-
+import subprocess
+from gtts import gTTS
+from gtts.utils import get_filename
 from autogpt.speech.base import VoiceBase
-
 
 class GTTSVoice(VoiceBase):
     """GTTS Voice."""
 
+    def __init__(self):
+        super().__init__()
+
     def _setup(self) -> None:
         pass
 
-    def _speech(self, text: str, _: int = 0) -> bool:
+    def _speech(self, text: str, rate: int = 200) -> bool:
         """Play the given text."""
-        tts = gtts.gTTS(text)
-        tts.save("speech.mp3")
-        playsound("speech.mp3", True)
-        os.remove("speech.mp3")
+        tts = gTTS(text, lang="en", slow=False, tld="com.au", rate=rate)
+        filename = get_filename(text)
+        tts.save(filename)
+        try:
+            subprocess.run(["afplay", filename])
+        except FileNotFoundError:
+            print("afplay not found, install it to play audio.")
+        finally:
+            os.remove(filename)
         return True
