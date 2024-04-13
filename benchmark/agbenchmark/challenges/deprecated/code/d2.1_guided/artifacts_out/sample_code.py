bf46cd4ad30aaf96d3b13db8c5ea42e6abc393f9
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:46:48 +0530

    improve

--- benchmark/agbenchmark/challenges/deprecated/code/d2.1_guided/artifacts_out/sample_code.py
+++ benchmark/agbenchmark/challenges/deprecated/code/d2.1_guided/artifacts_out/sample_code.py
@@ -1,7 +1,19 @@
 from typing import List, Optional
 
+def two_sum(nums: List[int], target: int) -> Optional[List[int]]:
+    """
+    Given an array of integers `nums` and an integer `target`, return
+    indices of the two numbers such that they add up to `target`.
 
-def two_sum(nums: List, target: int) -> Optional[List[int]]:
+    If there are no two numbers that add up to `target`, return `None`.
+
+    Parameters:
+    nums (List[int]): list of integers
+    target (int): target integer
+
+    Returns:
+    Optional[List[int]]: list of two indices that add up to target, or None
+    """
     seen = {}
     for i, num in enumerate(nums):
         complement = target - num
