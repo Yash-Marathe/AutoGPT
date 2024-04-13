58f56b4093bff79fafb9882f0cb495f1683025c4
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:46:52 +0530

    improve

--- benchmark/agbenchmark/challenges/verticals/code/6_battleship/artifacts_in/conftest.py
+++ benchmark/agbenchmark/challenges/verticals/code/6_battleship/artifacts_in/conftest.py
@@ -9,53 +9,33 @@ def battleship_game():
 
 
 @pytest.fixture
-def initialized_game_id(battleship_game):
-    # Create a game instance
-    game_id = battleship_game.create_game()
+def initialize_game(battleship_game):
+    def game_initializer(ship_placements=None):
+        # Create a game instance
+        game_id = battleship_game.create_game()
 
-    # Place all the ships using battleship_game's methods
-    sample_ship_placements = [
-        ShipPlacement(
-            ship_type="carrier", start={"row": 1, "column": "A"}, direction="horizontal"
-        ),
-        ShipPlacement(
-            ship_type="battleship",
-            start={"row": 2, "column": "A"},
-            direction="horizontal",
-        ),
-        ShipPlacement(
-            ship_type="cruiser", start={"row": 3, "column": "A"}, direction="horizontal"
-        ),
-        ShipPlacement(
-            ship_type="submarine",
-            start={"row": 4, "column": "A"},
-            direction="horizontal",
-        ),
-        ShipPlacement(
-            ship_type="destroyer",
-            start={"row": 5, "column": "A"},
-            direction="horizontal",
-        ),
-    ]
+        # Place all the ships using battleship_game's methods
+        if ship_placements is None:
+            ship_placements = [
+                ShipPlacement(ship_type, (row, column), "horizontal")
+                for row, column, ship_type in [
+                    (1, "A", "carrier"),
+                    (2, "A", "battleship"),
+                    (3, "A", "cruiser"),
+                    (4, "A", "submarine"),
+                    (5, "A", "destroyer"),
+                ]
+            ]
 
-    for ship_placement in sample_ship_placements:
-        # Place ship using battleship_game's methods
-        battleship_game.create_ship_placement(game_id, ship_placement)
+        for ship_placement in ship_placements:
+            battleship_game.create_ship_placement(game_id, ship_placement)
 
-    return game_id
+        return game_id
+
+    return game_initializer
 
 
 @pytest.fixture
-def game_over_fixture(battleship_game, initialized_game_id):
-    # Assuming 10x10 grid, target all possible positions
-    for row in range(1, 11):
-        for column in list("ABCDEFGHIJ"):
-            # Player 1 takes a turn
-            turn = Turn(target={"row": row, "column": column})
-            battleship_game.create_turn(initialized_game_id, turn)
-
-            # Player 2 takes a turn, targeting the same position as Player 1
-            battleship_game.create_turn(initialized_game_id, turn)
-
-    # At the end of this fixture, the game should be over
-    return initialized_game_id
+def game_over_fixture(battleship_game, initialize_game):
+    def game_over(game_id, width=10, height=10):
+        # Assuming 10x1
