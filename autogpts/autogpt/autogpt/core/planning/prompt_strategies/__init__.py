c230e738a4f4b8383f904c1b4ec7bce59cc270e3
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:46:31 +0530

    improve

--- autogpts/autogpt/autogpt/core/planning/prompt_strategies/__init__.py
+++ autogpts/autogpt/autogpt/core/planning/prompt_strategies/__init__.py
@@ -1,6 +1,8 @@
-from .initial_plan import InitialPlan, InitialPlanConfiguration
-from .name_and_goals import NameAndGoals, NameAndGoalsConfiguration
-from .next_ability import NextAbility, NextAbilityConfiguration
+"""Module providing various plan configurations and related classes."""
+
+from initial_plan import InitialPlan, InitialPlanConfiguration
+from name_and_goals import NameAndGoals, NameAndGoalsConfiguration
+from next_ability import NextAbility, NextAbilityConfiguration
 
 __all__ = [
     "InitialPlan",
