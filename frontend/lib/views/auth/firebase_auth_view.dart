8478053c17830fac89c1df48f95cec81a7de868e
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:59 +0530

    improve

--- frontend/lib/views/auth/firebase_auth_view.dart
+++ frontend/lib/views/auth/firebase_auth_view.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:auto_gpt_flutter_client/services/auth_service.dart';
 
 class FirebaseAuthView extends StatelessWidget {
-  // TODO: This should be initialized in the main.dart instead of here
-  final AuthService _authService = AuthService();
-
-  FirebaseAuthView({super.key}); // Initialize the auth service
+  const FirebaseAuthView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context, listen: false);
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -16,7 +16,7 @@ class FirebaseAuthView extends StatelessWidget {
           children: <Widget>[
             OutlinedButton(
               onPressed: () async {
-                final user = await _authService.signInWithGoogle();
+                final user = await authService.signInWithGoogle();
                 if (user != null) {
                   print(
                       "Successfully signed in with Google: ${user.user?.displayName}");
@@ -31,7 +31,7 @@ class FirebaseAuthView extends StatelessWidget {
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Image.asset('assets/images/google_logo.svg.png', width: 24),
+                  Image.asset('assets/images/google_logo.png', width: 24),
                   const SizedBox(width: 8),
                   const Text('Sign in with Google',
                       style: TextStyle(fontWeight: FontWeight.w300)),
@@ -41,7 +41,7 @@ class FirebaseAuthView extends StatelessWidget {
             const SizedBox(height: 20),
             OutlinedButton(
               onPressed: () async {
-                final user = await _authService.signInWithGitHub();
+                final user = await authService.signInWithGitHub();
                 if (user != null) {
                   print(
                       "Successfully signed in with GitHub: ${user.user?.displayName}");
@@ -49,23 +49,4 @@ class FirebaseAuthView extends StatelessWidget {
               },
               style: OutlinedButton.styleFrom(
                 foregroundColor: Colors.black,
-                side: const BorderSide(color: Colors.black, width: 2),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset('assets/images/github_logo.svg.png', width: 24),
-                  const SizedBox(width: 8),
-                  const Text('Sign in with GitHub',
-                      style: TextStyle(fontWeight: FontWeight.w300)),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+              
