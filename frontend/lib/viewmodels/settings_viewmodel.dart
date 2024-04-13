fae27ea4b4faa1e87076a5462444b3f3e92831db
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:49:01 +0530

    improve

--- frontend/lib/viewmodels/settings_viewmodel.dart
+++ frontend/lib/viewmodels/settings_viewmodel.dart
@@ -3,59 +3,46 @@ import 'package:auto_gpt_flutter_client/services/shared_preferences_service.dart
 import 'package:auto_gpt_flutter_client/utils/rest_api_utility.dart';
 import 'package:flutter/material.dart';
 
-/// [SettingsViewModel] is responsible for managing the state and logic
-/// for the [SettingsView]. It extends [ChangeNotifier] to provide
-/// reactive state management.
 class SettingsViewModel extends ChangeNotifier {
-  bool _isDarkModeEnabled = false; // State for Dark Mode
-  bool _isDeveloperModeEnabled = false; // State for Developer Mode
-  String _baseURL = ''; // State for Base URL
-  int _continuousModeSteps = 1; // State for Continuous Mode Steps
+  bool _isDarkModeEnabled;
+  bool _isDeveloperModeEnabled;
+  String _baseURL;
+  int _continuousModeSteps;
 
   final RestApiUtility _restApiUtility;
   final SharedPreferencesService _prefsService;
+  final AuthService _authService;
 
-  // Getters to access the private state variables
+  SettingsViewModel(this._restApiUtility, this._prefsService, this._authService) {
+    _loadPreferences();
+  }
+
   bool get isDarkModeEnabled => _isDarkModeEnabled;
   bool get isDeveloperModeEnabled => _isDeveloperModeEnabled;
   String get baseURL => _baseURL;
   int get continuousModeSteps => _continuousModeSteps;
 
-  final AuthService _authService = AuthService();
-
-  SettingsViewModel(this._restApiUtility, this._prefsService) {
-    _loadPreferences();
-  }
-
-  // Method to load stored preferences
   Future<void> _loadPreferences() async {
-    _isDarkModeEnabled =
-        await _prefsService.getBool('isDarkModeEnabled') ?? false;
-    _isDeveloperModeEnabled =
-        await _prefsService.getBool('isDeveloperModeEnabled') ?? true;
-    _baseURL = await _prefsService.getString('baseURL') ??
-        'http://127.0.0.1:8000/ap/v1';
+    _isDarkModeEnabled = await _prefsService.getBool('isDarkModeEnabled') ?? false;
+    _isDeveloperModeEnabled = await _prefsService.getBool('isDeveloperModeEnabled') ?? true;
+    _baseURL = await _prefsService.getString('baseURL') ?? 'http://127.0.0.1:8000/ap/v1';
     _restApiUtility.updateBaseURL(_baseURL);
-    _continuousModeSteps =
-        await _prefsService.getInt('continuousModeSteps') ?? 10;
+    _continuousModeSteps = await _prefsService.getInt('continuousModeSteps') ?? 10;
     notifyListeners();
   }
 
-  /// Toggles the state of Dark Mode and notifies listeners.
   Future<void> toggleDarkMode(bool value) async {
     _isDarkModeEnabled = value;
     notifyListeners();
     await _prefsService.setBool('isDarkModeEnabled', value);
   }
 
-  /// Toggles the state of Developer Mode and notifies listeners.
   Future<void> toggleDeveloperMode(bool value) async {
     _isDeveloperModeEnabled = value;
     notifyListeners();
     await _prefsService.setBool('isDeveloperModeEnabled', value);
   }
 
-  /// Updates the state of Base URL, notifies listeners, and updates the RestApiUtility baseURL.
   Future<void> updateBaseURL(String value) async {
     _baseURL = value;
     notifyListeners();
@@ -63,24 +50,28 @@ class SettingsViewModel extends ChangeNotifier {
     _restApiUtility.updateBaseURL(value);
   }
 
-  /// Increments the number of Continuous Mode Steps and notifies listeners.
-  Future<void> incrementContinuousModeSteps() async {
-    _continuousModeSteps += 1;
+  Future<void> updateContinuousModeSteps(int value) async {
+    _continuousModeSteps = value;
     notifyListeners();
-    await _prefsService.setInt('continuousModeSteps', _continuousModeSteps);
+    await _prefsService.setInt('continuousModeSteps', value);
   }
 
-  /// Decrements the number of Continuous Mode Steps and notifies listeners.
-  Future<void> decrementContinuousModeSteps() async {
-    if (_continuousModeSteps > 1) {
-      // Ensure that the number of steps is at least 1
-      _continuousModeSteps -= 1;
+  Future<void> incrementContinuousModeSteps() async {
+    if (_continuousModeSteps < 100) {
+      _continuousModeSteps++;
       notifyListeners();
       await _prefsService.setInt('continuousModeSteps', _continuousModeSteps);
     }
   }
 
-  // Method to sign out
+  Future<void> decrementContinuousModeSteps() async {
+    if (_continuousModeSteps > 1) {
+      _continuousModeSteps--;
+      notifyListeners();
+      await _prefsService.setInt('continuousModeSteps', _continuousModeSteps);
+    }
+  }
+
   Future<void> signOut() async {
     await _authService.signOut();
   }
