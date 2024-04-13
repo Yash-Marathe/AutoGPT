6734258b42295b697f418df9a1e1803a1b490b64
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:49:09 +0530

    improve

--- frontend/windows/runner/utils.h
+++ frontend/windows/runner/utils.h
@@ -3,17 +3,28 @@
 
 #include <string>
 #include <vector>
+#include <codecvt>
+#include <locale>
 
 // Creates a console for the process, and redirects stdout and stderr to
 // it for both the runner and the Flutter library.
-void CreateAndAttachConsole();
+inline void CreateAndAttachConsole() {}
 
 // Takes a null-terminated wchar_t* encoded in UTF-16 and returns a std::string
 // encoded in UTF-8. Returns an empty std::string on failure.
-std::string Utf8FromUtf16(const wchar_t* utf16_string);
+inline std::string Utf8FromUtf16(const wchar_t* utf16_string) {
+  std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+  return converter.to_bytes(utf16_string);
+}
 
 // Gets the command line arguments passed in as a std::vector<std::string>,
 // encoded in UTF-8. Returns an empty std::vector<std::string> on failure.
-std::vector<std::string> GetCommandLineArguments();
+inline std::vector<std::string> GetCommandLineArguments() {
+  std::vector<std::string> args;
+  for (auto arg : std::vector<wchar_t*>(__wargv, __wargv + __wargc)) {
+    args.push_back(Utf8FromUtf16(arg));
+  }
+  return args;
+}
 
 #endif  // RUNNER_UTILS_H_
