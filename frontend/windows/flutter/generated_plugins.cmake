66d71f816754cb990d4ec45cece20093ca335deb
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:49:00 +0530

    improve

--- frontend/windows/flutter/generated_plugins.cmake
+++ frontend/windows/flutter/generated_plugins.cmake
@@ -1,6 +1,4 @@
-#
 # Generated file, do not edit.
-#
 
 list(APPEND FLUTTER_PLUGIN_LIST
   firebase_core
@@ -16,10 +14,21 @@ foreach(plugin ${FLUTTER_PLUGIN_LIST})
   add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/windows plugins/${plugin})
   target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
   list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
+  
+  # Include bundled libraries for the plugin
+  foreach(lib ${${plugin}_bundled_libraries})
+    list(APPEND PLUGIN_BUNDLED_LIBRARIES ${lib})
+  endforeach()
 endforeach(plugin)
 
 foreach(ffi_plugin ${FLUTTER_FFI_PLUGIN_LIST})
   add_subdirectory(flutter/ephemeral/.plugin_symlinks/${ffi_plugin}/windows plugins/${ffi_plugin})
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${ffi_plugin}_bundled_libraries})
+  
+  # Include bundled libraries for the FFI plugin
+  foreach(lib ${${ffi_plugin}_bundled_libraries})
+    list(APPEND PLUGIN_BUNDLED_LIBRARIES ${lib})
+  endforeach()
 endforeach(ffi_plugin)
+
+# Print the list of bundled libraries for debugging purposes
+message(STATUS "Bundled libraries: ${PLUGIN_BUNDLED_LIBRARIES}")
