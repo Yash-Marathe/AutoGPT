ab79282f4c25374c8e305942424a205695b75dfc
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:09 +0530

    improve

--- frontend/macos/Runner/AppDelegate.swift
+++ frontend/macos/Runner/AppDelegate.swift
@@ -1,9 +1,17 @@
 import Cocoa
 import FlutterMacOS
 
-@NSApplicationMain
+@main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        // Return true to allow the application to terminate after the last window is closed
+        // Return false if you want the application to continue running even after the last window is closed
+        return true
+    }
+    
+    override func configureFlutterEngine(_ engine: FlutterEngine) {
+        super.configureFlutterEngine(engine)
+        
+        // Configure additional Flutter engine settings here, if needed
+    }
 }
