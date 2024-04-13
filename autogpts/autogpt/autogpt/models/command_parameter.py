00b2b9a51e0e2ec5c8f08cb26caaee9202dc8d58
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:45:27 +0530

    improve

--- autogpts/autogpt/autogpt/models/command_parameter.py
+++ autogpts/autogpt/autogpt/models/command_parameter.py
@@ -1,4 +1,5 @@
 import dataclasses
+from typing import Optional
 
 from autogpt.core.utils.json_schema import JSONSchema
 
@@ -7,11 +8,17 @@ from autogpt.core.utils.json_schema import JSONSchema
 class CommandParameter:
     name: str
     spec: JSONSchema
+    description: Optional[str] = None
+    required: Optional[bool] = None
+
+    def __post_init__(self):
+        if self.description is None:
+            self.description = self.spec.description
+        if self.required is None:
+            self.required = self.spec.required
 
     def __repr__(self):
-        return "CommandParameter('%s', '%s', '%s', %s)" % (
-            self.name,
-            self.spec.type,
-            self.spec.description,
-            self.spec.required,
+        return (
+            f"CommandParameter({self.name!r}, {self.spec.type!r}, "
+            f"{self.description!r}, {self.required!r})"
         )
