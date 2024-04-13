1739694e0e97bbf424514625fa4fdff497137262
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:46 +0530

    improve

--- frontend/lib/utils/stack.dart
+++ frontend/lib/utils/stack.dart
@@ -6,15 +6,32 @@ class Stack<T> {
   }
 
   T pop() {
+    if (isEmpty) {
+      throw StateError('Cannot pop from an empty stack');
+    }
     var element = _list.last;
     _list.removeLast();
     return element;
   }
 
   T peek() {
+    if (isEmpty) {
+      throw StateError('Cannot peek at an empty stack');
+    }
     return _list.last;
   }
 
   bool get isEmpty => _list.isEmpty;
   bool get isNotEmpty => _list.isNotEmpty;
+
+  int get length => _list.length;
+
+  void clear() {
+    _list.clear();
+  }
+
+  @override
+  String toString() {
+    return 'Stack(${_list.join(', ')})';
+  }
 }
