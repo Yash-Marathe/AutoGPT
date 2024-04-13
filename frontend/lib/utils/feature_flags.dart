81d458e7e8dcae21219e0cf2d5ed303aeb5bae2b
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:20 +0530

    improve

--- frontend/lib/utils/feature_flags.dart
+++ frontend/lib/utils/feature_flags.dart
@@ -1,3 +1,30 @@
+// FeatureFlags.dart
+
 class FeatureFlags {
-  static const bool userExperienceIterationTwoEnabled = false;
+  static const bool userExperienceIterationTwoEnabled = bool.fromEnvironment('USER_EXPERIENCE_ITERATION_TWO_ENABLED', defaultValue: false);
 }
+
+
+
+// main.dart
+
+import 'FeatureFlags.dart';
+
+void main() {
+  print(FeatureFlags.userExperienceIterationTwoEnabled);
+}
+
+
+
+// Terminal
+
+$ flutter run --dart-define=USER_EXPERIENCE_ITERATION_TWO_ENABLED=true
+
+
+
+class FeatureFlags {
+  static const bool userExperienceIterationTwoEnabled = bool.fromEnvironment('USER_EXPERIENCE_ITERATION_TWO_ENABLED', defaultValue: false);
+  static const bool premiumFeaturesEnabled = bool.fromEnvironment('PREMIUM_FEATURES_ENABLED', defaultValue: false);
+  static const bool analyticsEnabled = bool.fromEnvironment('ANALYTICS_ENABLED', defaultValue: true);
+}
+
