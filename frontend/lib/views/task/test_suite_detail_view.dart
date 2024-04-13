8c19f90a78188ec38d1df3df9de8b76be7f723ec
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:23 +0530

    improve

--- frontend/lib/views/task/test_suite_detail_view.dart
+++ frontend/lib/views/task/test_suite_detail_view.dart
@@ -1,18 +1,104 @@
-import 'package:auto_gpt_flutter_client/models/test_suite.dart';
-import 'package:auto_gpt_flutter_client/viewmodels/chat_viewmodel.dart';
-import 'package:auto_gpt_flutter_client/viewmodels/task_viewmodel.dart';
-import 'package:auto_gpt_flutter_client/views/task/task_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// TODO: Do we want a view model for every view?
+class TaskListTile extends StatefulWidget {
+  final String id;
+  final String title;
+  final bool completed;
+  final Function(String) onTap;
+  final Function(String) onDelete;
+  final Function(String, bool) onChanged;
+  final bool selected;
+
+  const TaskListTile({
+    Key? key,
+    required this.id,
+    required this.title,
+    required this.completed,
+    required this.onTap,
+    required this.onDelete,
+    required this.onChanged,
+    required this.selected,
+  }) : super(key: key);
+
+  @override
+  _TaskListTileState createState() => _TaskListTileState();
+}
+
+class _TaskListTileState extends State<TaskListTile> {
+  bool _checked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checked = widget.completed;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      key: ValueKey(widget.id),
+      leading: Checkbox(
+        value: _checked,
+        onChanged: (value) {
+          setState(() {
+            _checked = value!;
+          });
+          widget.onChanged(widget.id, _checked);
+        },
+      ),
+      title: Text(widget.title),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                widget.onDelete(widget.id);
+              },
+            ),
+            Consumer<ChatViewModel>(
+              builder: (context, chatViewModel, child) {
+                return Visibility(
+                  visible: widget.id == chatViewModel.currentTaskId,
+                  child: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      chatViewModel.clearCurrentTaskAndChats();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        widget.onTap(widget.id);
+      },
+      selected: widget.selected,
+    );
+  }
+}
+
+
+import 'package:auto_gpt_flutter_client/models/test_suite.dart';
+import 'package:auto_gpt_flutter_client/viewmodels/chat_viewmodel.dart';
+import 'package:auto_gpt_flutter_client/viewmodels/task_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 class TestSuiteDetailView extends StatefulWidget {
   final TaskViewModel viewModel;
   final TestSuite testSuite;
 
-  const TestSuiteDetailView(
-      {Key? key, required this.testSuite, required this.viewModel})
-      : super(key: key);
+  const TestSuiteDetailView({
+    Key? key,
+    required this.testSuite,
+    required this.viewModel,
+  }) : super(key: key);
 
   @override
   _TestSuiteDetailViewState createState() => _TestSuiteDetailViewState();
@@ -37,43 +123,7 @@ class _TestSuiteDetailViewState extends State<TestSuiteDetailView> {
           // Task List
           Expanded(
             child: ListView.builder(
-              itemCount:
-                  widget.testSuite.tests.length, // Count of tasks passed in
+              itemCount: widget.testSuite.tests.length,
               itemBuilder: (context, index) {
                 final task = widget.testSuite.tests[index];
-                return TaskListTile(
-                  task: task,
-                  onTap: () {
-                    // Select the task in TaskViewModel
-                    widget.viewModel.selectTask(task.id);
-
-                    // Update the current task ID in ChatViewModel
-                    // TODO: Do we want to have a reference to chat view model in this class?
-                    final chatViewModel =
-                        Provider.of<ChatViewModel>(context, listen: false);
-                    chatViewModel.setCurrentTaskId(task.id);
-
-                    print('Task ${task.title} tapped');
-                  },
-                  onDelete: () {
-                    // Delete the task in TaskViewModel
-                    widget.viewModel.deleteTask(task.id);
-                    // TODO: Do we want to have a reference to chat view model in this class?
-                    final chatViewModel =
-                        Provider.of<ChatViewModel>(context, listen: false);
-                    if (chatViewModel.currentTaskId == task.id) {
-                      chatViewModel.clearCurrentTaskAndChats();
-                    }
-
-                    print('Task ${task.title} delete button tapped');
-                  },
-                  selected: task.id == widget.viewModel.selectedTask?.id,
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+                return Cons
