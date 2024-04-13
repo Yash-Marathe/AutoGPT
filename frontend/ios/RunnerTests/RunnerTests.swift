f066e4ebb4923499674d2acf98ee1b3c3ec36aca
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:53 +0530

    improve

--- frontend/ios/RunnerTests/RunnerTests.swift
+++ frontend/ios/RunnerTests/RunnerTests.swift
@@ -1,12 +1,31 @@
-import Flutter
-import UIKit
 import XCTest
+@testable import Runner
 
 class RunnerTests: XCTestCase {
 
-  func testExample() {
-    // If you add code to the Runner application, consider adding tests here.
-    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
-  }
+    var appDelegate: AppDelegate!
+    var window: UIWindow!
+
+    override func setUp() {
+        super.setUp()
+        
+        // Use these lines to set up your UI tests
+        appDelegate = UIApplication.shared.delegate as? AppDelegate
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window.makeKeyAndVisible()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationController = storyboard.instantiateViewController(withIdentifier: "NavigationController")
+        appDelegate?.window??.rootViewController = navigationController
+    }
+
+    func testExample() {
+        // If you add code to the Runner application, consider adding tests here.
+        // Use this method to test basic UI functionality of your app
+        let firstViewController = appDelegate.window??.rootViewController as? FirstViewController
+        XCTAssertNotNil(firstViewController)
+        
+        firstViewController?.someMethod()
+        XCTAssertTrue(firstViewController?.someProperty ?? false)
+    }
 
 }
