2b0fadd918cd43fe7b0f90ec6ff86e324712b5da
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:47:54 +0530

    improve

--- docs/_javascript/tablesort.js
+++ docs/_javascript/tablesort.js
@@ -1,6 +1,10 @@
-document$.subscribe(function () {
-    var tables = document.querySelectorAll("article table:not([class])")
-    tables.forEach(function (table) {
-        new Tablesort(table)
-    })
-})
+document$.subscribe(() => {
+  const tables = Array.from(document.querySelectorAll("article table:not([class])"));
+  tables.forEach(table => new Tablesort(table));
+});
+
+
+document$.subscribe(() => {
+  const tables = Array.from(document.querySelectorAll("article table:not([class])"));
+  tables.forEach(Tablesort);
+});
