2c2559a0ce8c4ec8001323b412233bacb8c16368
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:46:47 +0530

    improve

--- benchmark/agbenchmark/challenges/deprecated/code/1_return/artifacts_in/sample_code.py
+++ benchmark/agbenchmark/challenges/deprecated/code/1_return/artifacts_in/sample_code.py
@@ -1,2 +1,6 @@
 def multiply_int(num: int) -> int:
+    """
+    This function takes an integer as input and returns the result of multiplying it by 2.
+    """
     multiplied_num = num * 2
+    return multiplied_num
