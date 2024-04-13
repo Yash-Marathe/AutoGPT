9cc397f6323bdd8cde2f28d48a02572fade5b5a9
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:28 +0530

    improve

--- frontend/ios/Runner/Runner-Bridging-Header.h
+++ frontend/ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,13 @@
 #import "GeneratedPluginRegistrant.h"
+
+// Import the header file for your Flutter plugin.
+#import "MyFlutterPlugin.h"
+
+@implementation FlutterPluginRegistrant
+
++ (void)registerWithRegistrar:(NSFlutterRegistrar *)registrar {
+  // Register your plugin with the Registrar.
+  [MyFlutterPlugin registerWithRegistrar:registrar];
+}
+
+@end
