cdaa5490ad529b2fe3d17c68444b37fa88f8f1f9
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:49:33 +0530

    improve

--- frontend/lib/models/benchmark/benchmark_step_request_body.dart
+++ frontend/lib/models/benchmark/benchmark_step_request_body.dart
@@ -1,12 +1,26 @@
+import 'dart:convert';
+
 class BenchmarkStepRequestBody {
   final String? input;
 
   BenchmarkStepRequestBody({required this.input});
 
   Map<String, dynamic> toJson() {
-    if (input == null) {
-      return {};
+    Map<String, dynamic> jsonMap = {};
+    if (input != null) {
+      jsonMap['input'] = input;
     }
-    return {'input': input};
+    return jsonMap;
+  }
+
+  /// Returns a JSON-encoded string version of this object.
+  String toJsonString() => json.encode(toJson());
+
+  /// Creates a BenchmarkStepRequestBody object from a JSON-encoded string.
+  factory BenchmarkStepRequestBody.fromJsonString(String jsonString) {
+    final jsonMap = json.decode(jsonString);
+    return BenchmarkStepRequestBody(
+      input: jsonMap['input'] as String?,
+    );
   }
 }
