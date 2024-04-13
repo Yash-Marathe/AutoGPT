17260243f7b67d5718d5229ca7eab5bbdc6f5e90
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:14 +0530

    improve

--- frontend/linux/main.cc
+++ frontend/linux/main.cc
@@ -1,6 +1,22 @@
+#include <glib.h>
 #include "my_application.h"
 
-int main(int argc, char** argv) {
-  g_autoptr(MyApplication) app = my_application_new();
-  return g_application_run(G_APPLICATION(app), argc, argv);
+int
+main (int argc, char **argv)
+{
+  g_autoptr(MyApplication) app = my_application_new ();
+  gint status;
+
+  /* Check if the application is already running */
+  if (!g_application_is_initialized (G_APPLICATION (app)))
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+  else
+    {
+      /* Get the existing instance and send a message to it */
+      GApplication *existing = g_application_get_default ();
+      g_application_activate (existing, argc, argv);
+      status = 0;
+    }
+
+  return status;
 }
