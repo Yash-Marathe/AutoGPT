b1f34388c5e83924f05fdafa8ad7fc7a9a3975bc
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:49:18 +0530

    improve

--- frontend/test/step_request_body_test.dart
+++ frontend/test/step_request_body_test.dart
@@ -1,26 +1,38 @@
 import 'package:auto_gpt_flutter_client/models/step_request_body.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'dart:convert';
 
 void main() {
   group('StepRequestBody', () {
-    test('should create StepRequestBody with correct values', () {
+    test('creates StepRequestBody with correct values', () {
       final stepRequestBody = StepRequestBody(
-          input: 'Execute something', additionalInput: {'key': 'value'});
+        input: 'Execute something',
+        additionalInput: {'key': 'value'},
+      );
 
       expect(stepRequestBody.input, 'Execute something');
       expect(stepRequestBody.additionalInput, {'key': 'value'});
     });
 
-    test('should convert StepRequestBody to correct JSON', () {
+    test('converts StepRequestBody to correct JSON', () {
       final stepRequestBody = StepRequestBody(
-          input: 'Execute something', additionalInput: {'key': 'value'});
+        input: 'Execute something',
+        additionalInput: {'key': 'value'},
+      );
 
-      final json = stepRequestBody.toJson();
+      final json = jsonEncode(stepRequestBody.toJson());
 
-      expect(json, {
-        'input': 'Execute something',
-        'additional_input': {'key': 'value'}
-      });
+      expect(json, '{"input":"Execute something","additional_input":{"key":"value"}}');
+    });
+
+    test('parses JSON to StepRequestBody', () {
+      const jsonString = '{"input":"Execute something","additional_input":{"key":"value"}}';
+      final json = jsonDecode(jsonString);
+
+      final stepRequestBody = StepRequestBody.fromJson(json);
+
+      expect(stepRequestBody.input, 'Execute something');
+      expect(stepRequestBody.additionalInput, {'key': 'value'});
     });
   });
 }
