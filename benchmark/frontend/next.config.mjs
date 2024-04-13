318fe409f6bf816961cfba75e9cca0ebc86f0f3a
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:47:34 +0530

    improve

--- benchmark/frontend/next.config.mjs
+++ benchmark/frontend/next.config.mjs
@@ -2,21 +2,32 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
 
-/** @type {import("next").NextConfig} */
-const config = {
+// Import the environment variables and validate them
+import "./src/env.mjs";
+
+/**
+ * The Next.js configuration object.
+ * @type {import("next").NextConfig}
+ */
+const nextConfig = {
+  // Enable strict mode for React components
   reactStrictMode: true,
 
   /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
+   * Internationalization configuration.
+   *
+   * Note: If you are using `appDir`, you must comment out the below `i18n` config.
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
+    // List of supported locales
     locales: ["en"],
+    // Default locale
     defaultLocale: "en",
   },
 };
 
-export default config;
+// Export the Next.js configuration object
+module.exports = nextConfig;
