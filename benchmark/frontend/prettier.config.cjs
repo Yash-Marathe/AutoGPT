7c5282d3ad64aab6c4df7215ff9758ceff79f6b9
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:46:55 +0530

    improve

--- benchmark/frontend/prettier.config.cjs
+++ benchmark/frontend/prettier.config.cjs
@@ -1,6 +1,10 @@
-/** @type {import("prettier").Config} */
-const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+// Importing Prettier's config type
+const { Config } = require("prettier");
+
+// Creating the Prettier config object
+const config: Config = {
+  plugins: [require("prettier-plugin-tailwindcss")],
 };
 
+// Exporting the config object
 module.exports = config;
