a276680029b7d921cb02a03e8c44aa332e1dc830
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:16 +0530

    improve

--- frontend/test/task_request_body_test.dart
+++ frontend/test/task_request_body_test.dart
@@ -1,26 +1,54 @@
-import 'package:auto_gpt_flutter_client/models/task_request_body.dart';
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:auto_gpt_flutter_client/models/task_request_body.dart';
 
 void main() {
   group('TaskRequestBody', () {
-    test('should create TaskRequestBody with correct values', () {
+    test('creates TaskRequestBody with correct values', () {
       final taskRequestBody = TaskRequestBody(
-          input: 'Do something', additionalInput: {'key': 'value'});
+        input: 'Do something',
+        additionalInput: {'key': 'value'},
+      );
 
       expect(taskRequestBody.input, 'Do something');
       expect(taskRequestBody.additionalInput, {'key': 'value'});
     });
 
-    test('should convert TaskRequestBody to correct JSON', () {
+    test('converts TaskRequestBody to correct JSON', () {
       final taskRequestBody = TaskRequestBody(
-          input: 'Do something', additionalInput: {'key': 'value'});
+        input: 'Do something',
+        additionalInput: {'key': 'value'},
+      );
 
-      final json = taskRequestBody.toJson();
+      final json = jsonEncode(taskRequestBody);
 
-      expect(json, {
-        'input': 'Do something',
-        'additional_input': {'key': 'value'}
-      });
+      final expectedJson = '''
+      {
+        "input": "Do something",
+        "additional_input": {
+          "key": "value"
+        }
+      }
+      ''';
+
+      expect(json, expectedJson);
+    });
+
+    test('parses JSON to TaskRequestBody', () {
+      final json = '''
+      {
+        "input": "Do something",
+        "additional_input": {
+          "key": "value"
+        }
+      }
+      ''';
+
+      final taskRequestBody = TaskRequestBody.fromJson(jsonDecode(json));
+
+      expect(taskRequestBody.input, 'Do something');
+      expect(taskRequestBody.additionalInput, {'key': 'value'});
     });
   });
 }
