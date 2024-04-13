d915a1be4cec6c8656da21e6704993a2340d6400
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:49:12 +0530

    improve

--- frontend/lib/models/test_option.dart
+++ frontend/lib/models/test_option.dart
@@ -1,7 +1,6 @@
 /// `TestOption` is an enumeration of the available test options that can be selected in the skill tree view.
 ///
 /// Each value of this enum represents a distinct test option that can be executed.
-/// The `description` getter can be used to get the string representation of each test option.
 enum TestOption {
   /// Represents the option to run a single test.
   runSingleTest,
@@ -23,24 +22,7 @@ extension TestOptionExtension on TestOption {
   /// Returns a human-readable string that describes the test option. This string is intended
   /// to be displayed in the UI for user selection.
   String get description {
-    switch (this) {
-      /// In case of a single test option, return the corresponding string.
-      case TestOption.runSingleTest:
-        return 'Run single test';
-
-      /// In case of a test suite option that includes selected node and ancestors, return the corresponding string.
-      case TestOption.runTestSuiteIncludingSelectedNodeAndAncestors:
-        return 'Run test suite including selected node and ancestors';
-
-      /// In case of an option to run all tests in a category, return the corresponding string.
-      case TestOption.runAllTestsInCategory:
-        return 'Run all tests in category';
-
-      /// In case of an undefined or unknown test option, return a generic unknown string.
-      /// This case should ideally never be hit if all enum values are handled.
-      default:
-        return 'Unknown';
-    }
+    return describeEnum(this);
   }
 
   /// Converts a [description] string to its corresponding [TestOption] enum value.
@@ -51,15 +33,6 @@ extension TestOptionExtension on TestOption {
   ///
   /// Returns the matching [TestOption] enum value if found, otherwise returns `null`.
   static TestOption? fromDescription(String description) {
-    switch (description) {
-      case 'Run single test':
-        return TestOption.runSingleTest;
-      case 'Run test suite including selected node and ancestors':
-        return TestOption.runTestSuiteIncludingSelectedNodeAndAncestors;
-      case 'Run all tests in category':
-        return TestOption.runAllTestsInCategory;
-      default:
-        return null; // or throw an exception, or provide a default value, as per your requirement
-    }
+    return TestOption.values.firstWhereOrNull((option) => option.description == description);
   }
 }
