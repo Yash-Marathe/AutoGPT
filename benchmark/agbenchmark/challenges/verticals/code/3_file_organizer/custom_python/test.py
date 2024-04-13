10f77d10991fa2d37e0e4b9f6e2a85614a712ecb
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:47:15 +0530

    improve

--- benchmark/agbenchmark/challenges/verticals/code/3_file_organizer/custom_python/test.py
+++ benchmark/agbenchmark/challenges/verticals/code/3_file_organizer/custom_python/test.py
@@ -1,8 +1,7 @@
 import os
 import subprocess
-import tempfile
+import shutil
 import unittest
-
 
 class TestOrganizeFiles(unittest.TestCase):
     def setUp(self):
@@ -18,28 +17,7 @@ class TestOrganizeFiles(unittest.TestCase):
 
         # Create test files
         for file_name in self.file_types.keys():
-            open(os.path.join(self.test_dir, file_name), "a").close()
+            file_path = os.path.join(self.test_dir, file_name)
+            with open(file_path, "w") as f:
+                f.write("Test content")
 
-    def test_organize_files(self):
-        # Call the organize_files.py script using subprocess
-        subprocess.call(
-            ["python", "organize_files.py", "--directory_path=" + self.test_dir]
-        )
-
-        # Check if the files have been moved to the correct directories
-        for file_name, directory in self.file_types.items():
-            self.assertTrue(
-                os.path.isfile(os.path.join(self.test_dir, directory, file_name))
-            )
-
-    def tearDown(self):
-        # Delete test directory and its contents
-        for file_name, directory in self.file_types.items():
-            os.remove(os.path.join(self.test_dir, directory, file_name))
-        for directory in set(self.file_types.values()):
-            os.rmdir(os.path.join(self.test_dir, directory))
-        os.rmdir(self.test_dir)
-
-
-if __name__ == "__main__":
-    unittest.main()
