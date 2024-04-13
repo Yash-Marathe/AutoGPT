c6bf5ed7cd114045a4de5888f60d6c3f4eadc532
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:49:34 +0530

    improve

--- frontend/lib/models/task_request_body.dart
+++ frontend/lib/models/task_request_body.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class TaskRequestBody {
   final String input;
   final Map<String, dynamic>? additionalInput;
@@ -5,6 +7,30 @@ class TaskRequestBody {
   TaskRequestBody({required this.input, this.additionalInput});
 
   Map<String, dynamic> toJson() {
-    return {'input': input, 'additional_input': additionalInput};
+    final Map<String, dynamic> jsonMap = {'input': input};
+    if (additionalInput != null) {
+      jsonMap['additional_input'] = additionalInput;
+    }
+    return jsonMap;
+  }
+
+  /// Returns a TaskRequestBody object constructed from a JSON object
+  factory TaskRequestBody.fromJson(Map<String, dynamic> json) {
+    return TaskRequestBody(
+      input: json['input'],
+      additionalInput: json['additional_input'],
+    );
+  }
+
+  /// Returns a JSON string representation of this object
+  String toJsonString() {
+    final jsonBody = jsonEncode(toJson());
+    return jsonBody;
+  }
+
+  /// Creates a TaskRequestBody object from a JSON string
+  factory TaskRequestBody.fromJsonString(String jsonString) {
+    final jsonBody = jsonDecode(jsonString);
+    return TaskRequestBody.fromJson(jsonBody);
   }
 }
