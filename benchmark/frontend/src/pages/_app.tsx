aa0926bd9bcdb051341cdc84489a1ccf218ed2a3
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:47:08 +0530

    improve

--- benchmark/frontend/src/pages/_app.tsx
+++ benchmark/frontend/src/pages/_app.tsx
@@ -1,9 +1,11 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-import "~/styles/globals.css";
-import "@fortawesome/fontawesome-svg-core/styles.css";
+import type { AppProps } from 'next/app'
+import '~/styles/globals.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />
+}
 
-export default MyApp;
+export default MyApp
