8ebcf54aef8075ea261910dd60d7704502f9a1fd
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:49:15 +0530

    improve

--- frontend/windows/runner/utils.cpp
+++ frontend/windows/runner/utils.cpp
@@ -6,37 +6,50 @@
 #include <windows.h>
 
 #include <iostream>
+#include <vector>
 
-void CreateAndAttachConsole() {
-  if (::AllocConsole()) {
-    FILE *unused;
-    if (freopen_s(&unused, "CONOUT$", "w", stdout)) {
-      _dup2(_fileno(stdout), 1);
-    }
-    if (freopen_s(&unused, "CONOUT$", "w", stderr)) {
-      _dup2(_fileno(stdout), 2);
-    }
-    std::ios::sync_with_stdio();
-    FlutterDesktopResyncOutputStreams();
+bool CreateConsoleIfNecessary() {
+  if (_isatty(_fileno(stdout)) && _isatty(_fileno(stderr))) {
+    // The process already has a console window.
+    return false;
   }
+
+  if (!::AllocConsole()) {
+    // Failed to create a console window.
+    std::cerr << "Failed to create a console window: " << ::GetLastError() << std::endl;
+    return false;
+  }
+
+  FILE* unused;
+  if (!freopen_s(&unused, "CONOUT$", "w", stdout)) {
+    _dup2(_fileno(stdout), 1);
+  } else {
+    std::cerr << "Failed to redirect stdout to the console window: " << ::GetLastError() << std::endl;
+    ::FreeConsole();
+    return false;
+  }
+
+  if (!freopen_s(&unused, "CONOUT$", "w", stderr)) {
+    _dup2(_fileno(stdout), 2);
+  } else {
+    std::cerr << "Failed to redirect stderr to the console window: " << ::GetLastError() << std::endl;
+    ::FreeConsole();
+    return false;
+  }
+
+  std::ios::sync_with_stdio();
+  FlutterDesktopResyncOutputStreams();
+
+  return true;
 }
 
-std::vector<std::string> GetCommandLineArguments() {
-  // Convert the UTF-16 command line arguments to UTF-8 for the Engine to use.
-  int argc;
-  wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
-  if (argv == nullptr) {
-    return std::vector<std::string>();
-  }
-
+std::vector<std::string> ConvertCommandLineArguments(int argc, wchar_t** argv) {
   std::vector<std::string> command_line_arguments;
 
   // Skip the first argument as it's the binary name.
   for (int i = 1; i < argc; i++) {
     command_line_arguments.push_back(Utf8FromUtf16(argv[i]));
   }
-
-  ::LocalFree(argv);
 
   return command_line_arguments;
 }
@@ -45,21 +58,5 @@ std::string Utf8FromUtf16(const wchar_t* utf16_string) {
   if (utf16_string == nullptr) {
     return std::string();
   }
-  int target_length = ::WideCharToMultiByte(
-      CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
-      -1, nullptr, 0, nullptr, nullptr)
-    -1; // remove the trailing null character
-  int input_length = (int)wcslen(utf16_string);
-  std::string utf8_string;
-  if (target_length <= 0 || target_length > utf8_string.max_size()) {
-    return utf8_string;
-  }
-  utf8_string.resize(target_length);
-  int converted_length = ::WideCharToMultiByte(
-      CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
-      input_length, utf8_string.data(), target_length, nullptr, nullptr);
-  if (converted_length == 0) {
-    return std::string();
-  }
-  return utf8_string;
-}
+
+
