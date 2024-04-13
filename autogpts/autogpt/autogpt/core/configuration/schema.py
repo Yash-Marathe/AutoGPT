63d7aa42ebc613ff5677f3f07d5dd2e8cc2aac4f
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:45:35 +0530

    improve

--- autogpts/autogpt/autogpt/core/configuration/schema.py
+++ autogpts/autogpt/autogpt/core/configuration/schema.py
@@ -1,60 +1,36 @@
-import abc
 import os
-import typing
-from typing import Any, Callable, Generic, Optional, Type, TypeVar, get_args
+from typing import Any, Callable, Dict, Optional
 
-from pydantic import BaseModel, Field, ValidationError
-from pydantic.fields import ModelField, Undefined, UndefinedType
-from pydantic.main import ModelMetaclass
-
-T = TypeVar("T")
-M = TypeVar("M", bound=BaseModel)
+import pydantic
 
 
-def UserConfigurable(
-    default: T | UndefinedType = Undefined,
-    *args,
-    default_factory: Optional[Callable[[], T]] = None,
-    from_env: Optional[str | Callable[[], T | None]] = None,
+def user_configurable(
+    default: Any = pydantic.Field(default=None, user_configurable=True),
+    default_factory: Optional[Callable[[], Any]] = None,
+    from_env: Optional[str | Callable[[], Any]] = None,
     description: str = "",
-    **kwargs,
-) -> T:
-    # TODO: use this to auto-generate docs for the application configuration
-    return Field(
-        default,
-        *args,
+) -> pydantic.Field:
+    return pydantic.Field(
+        default=default,
         default_factory=default_factory,
         from_env=from_env,
         description=description,
-        **kwargs,
         user_configurable=True,
     )
 
 
-class SystemConfiguration(BaseModel):
-    def get_user_config(self) -> dict[str, Any]:
+class SystemConfiguration(pydantic.BaseModel):
+    def get_user_config(self) -> Dict[str, Any]:
         return _recurse_user_config_values(self)
 
     @classmethod
     def from_env(cls):
-        """
-        Initializes the config object from environment variables.
-
-        Environment variables are mapped to UserConfigurable fields using the from_env
-        attribute that can be passed to UserConfigurable.
-        """
-
-        def infer_field_value(field: ModelField):
-            field_info = field.field_info
-            default_value = (
-                field.default
-                if field.default not in (None, Undefined)
-                else (field.default_factory() if field.default_factory else Undefined)
-            )
-            if from_env := field_info.extra.get("from_env"):
-                val_from_env = (
-                    os.getenv(from_env) if type(from_env) is str else from_env()
-                )
+        def infer_field_value(field: pydantic.fields.ModelField):
+            default_value = field.default
+            if default_value is pydantic.Undefined:
+                default_value = field.default_factory() if field.default_factory else None
+            if from_env := field.field_info.extra.get("from_env"):
+                val_from_env = os.getenv(from_env) if type(from_env) is str else from_env()
                 if val_from_env is not None:
                     return val_from_env
             return default_value
@@ -67,10 +43,7 @@ class SystemConfiguration(BaseModel):
         validate_assignment = True
 
 
-SC = TypeVar("SC", bound=SystemConfiguration)
-
-
-class SystemSettings(BaseModel):
+class SystemSettings(pydantic.BaseModel):
     """A base class for all system settings."""
 
     name: str
@@ -82,64 +55,35 @@ class SystemSettings(BaseModel):
         validate_assignment = True
 
 
-S = TypeVar("S", bound=SystemSettings)
-
-
-class Configurable(abc.ABC, Generic[S]):
-    """A base class for all configurable objects."""
-
+class Configurable:
     prefix: str = ""
-    default_settings: typing.ClassVar[S]
+    default_settings: Any
 
     @classmethod
-    def get_user_config(cls) -> dict[str, Any]:
+    def get_user_config(cls) -> Dict[str, Any]:
         return _recurse_user_config_values(cls.default_settings)
 
     @classmethod
-    def build_agent_configuration(cls, overrides: dict = {}) -> S:
-        """Process the configuration for this object."""
-
+    def build_agent_configuration(cls, overrides: dict = {}) -> Any:
         base_config = _update_user_config_from_env(cls.default_settings)
-        final_configuration = deep_update(base_config, overrides)
-
+        final_configuration = {**base_config, **overrides}
         return cls.default_settings.__class__.parse_obj(final_configuration)
 
 
-def _update_user_config_from_env(instance: BaseModel) -> dict[str, Any]:
-    """
-    Update config fields of a Pydantic model instance from environment variables.
-
-    Precedence:
-    1. Non-default value already on the instance
-    2. Value returned by `from_env()`
-    3. Default value for the field
-
-    Params:
-        instance: The Pydantic model instance.
-
-    Returns:
-        The user config fields of the instance.
-    """
-
-    def infer_field_value(field: ModelField, value):
-        field_info = field.field_info
-        default_value = (
-            field.default
-            if field.default not in (None, Undefined)
-            else (field.default_factory() if field.default_factory else None)
-        )
-        if value == default_value and (from_env := field_info.extra.get("from_env")):
+def _update_user_config_from_env(instance: pydantic.BaseModel) -> Dict[str, Any]:
+    def infer_field_value(field: pydantic.fields.ModelField, value):
+        default_value = field.default
+        if value == default_value and (from_env := field.field_info.extra.get("from_env")):
             val_from_env = os.getenv(from_env) if type(from_env) is str else from_env()
             if val_from_env is not None:
                 return val_from_env
         return value
 
-    def init_sub_config(model: Type[SC]) -> SC | None:
+    def init_sub_config(model: type) -> Any:
         try:
             return model.from_env()
-        except ValidationError as e:
-            # Gracefully handle missing fields
-            if all(e["type"] == "value_error.missing" for e in e.errors()):
+        except pydantic.ValidationError as e:
+            if all(e.errors()[0]["type"] == "value_error.missing" for e in e.errors()):
                 return None
             raise
 
@@ -147,77 +91,40 @@ def _update_user_config_from_env(instance: BaseModel) -> dict[str, Any]:
 
 
 def _recursive_init_model(
-    model: Type[M],
-    infer_field_value: Callable[[ModelField], Any],
-) -> M:
-    """
-    Recursively initialize the user configuration fields of a Pydantic model.
-
-    Parameters:
-        model: The Pydantic model type.
-        infer_field_value: A callback function to infer the value of each field.
-            Parameters:
-                ModelField: The Pydantic ModelField object describing the field.
-
-    Returns:
-        BaseModel: An instance of the model with the initialized configuration.
-    """
+    model: type, infer_field_value: Callable[[pydantic.fields.ModelField], Any]
+) -> Any:
     user_config_fields = {}
     for name, field in model.__fields__.items():
-        if "user_configurable" in field.field_info.extra:
+        if field.field_info.extra.get("user_configurable"):
             user_config_fields[name] = infer_field_value(field)
-        elif type(field.outer_type_) is ModelMetaclass and issubclass(
-            field.outer_type_, SystemConfiguration
-        ):
+        elif issubclass(field.outer_type_, SystemConfiguration):
             try:
                 user_config_fields[name] = _recursive_init_model(
-                    model=field.outer_type_,
-                    infer_field_value=infer_field_value,
+                    model=field.outer_type_, infer_field_value=infer_field_value
                 )
-            except ValidationError as e:
-                # Gracefully handle missing fields
-                if all(e["type"] == "value_error.missing" for e in e.errors()):
+            except pydantic.ValidationError as e:
+                if all(e.errors()[0]["type"] == "value_error.missing" for e in e.errors()):
                     user_config_fields[name] = None
                 raise
 
-    user_config_fields = remove_none_items(user_config_fields)
+    user_config_fields = {k: v for k, v in user_config_fields.items() if v is not None}
 
     return model.parse_obj(user_config_fields)
 
 
 def _recurse_user_config_fields(
-    model: BaseModel,
-    infer_field_value: Callable[[ModelField, Any], Any],
-    init_sub_config: Optional[
-        Callable[[Type[SystemConfiguration]], SystemConfiguration | None]
-    ] = None,
-) -> dict[str, Any]:
-    """
-    Recursively process the user configuration fields of a Pydantic model instance.
-
-    Params:
-        model: The Pydantic model to iterate over.
-        infer_field_value: A callback function to process each field.
-            Params:
-                ModelField: The Pydantic ModelField object describing the field.
-                Any: The current value of the field.
-        init_sub_config: An optional callback function to initialize a sub-config.
-            Params:
-                Type[SystemConfiguration]: The type of the sub-config to initialize.
-
-    Returns:
-        dict[str, Any]: The processed user configuration fields of the instance.
-    """
+    model: pydantic.BaseModel,
+    infer_field_value: Callable[[pydantic.fields.ModelField, Any], Any],
+    init_sub_config: Optional[Callable[[type], Any]] = None,
+) -> Dict[str, Any]:
     user_config_fields = {}
 
     for name, field in model.__fields__.items():
         value = getattr(model, name)
 
-        # Handle individual field
-        if "user_configurable" in field.field_info.extra:
+        if field.field_info.extra.get("user_configurable"):
             user_config_fields[name] = infer_field_value(field, value)
 
-        # Recurse into nested config object
         elif isinstance(value, SystemConfiguration):
             user_config_fields[name] = _recurse_user_config_fields(
                 model=value,
@@ -225,127 +132,5 @@ def _recurse_user_config_fields(
                 init_sub_config=init_sub_config,
             )
 
-        # Recurse into optional nested config object
-        elif value is None and init_sub_config:
-            field_type = get_args(field.annotation)[0]  # Optional[T] -> T
-            if type(field_type) is ModelMetaclass and issubclass(
-                field_type, SystemConfiguration
-            ):
-                sub_config = init_sub_config(field_type)
-                if sub_config:
-                    user_config_fields[name] = _recurse_user_config_fields(
-                        model=sub_config,
-                        infer_field_value=infer_field_value,
-                        init_sub_config=init_sub_config,
-                    )
-
-        elif isinstance(value, list) and all(
-            isinstance(i, SystemConfiguration) for i in value
-        ):
-            user_config_fields[name] = [
-                _recurse_user_config_fields(i, infer_field_value, init_sub_config)
-                for i in value
-            ]
-        elif isinstance(value, dict) and all(
-            isinstance(i, SystemConfiguration) for i in value.values()
-        ):
-            user_config_fields[name] = {
-                k: _recurse_user_config_fields(v, infer_field_value, init_sub_config)
-                for k, v in value.items()
-            }
-
-    return user_config_fields
-
-
-def _recurse_user_config_values(
-    instance: BaseModel,
-    get_field_value: Callable[[ModelField, T], T] = lambda _, v: v,
-) -> dict[str, Any]:
-    """
-    This function recursively traverses the user configuration values in a Pydantic
-    model instance.
-
-    Params:
-        instance: A Pydantic model instance.
-        get_field_value: A callback function to process each field. Parameters:
-            ModelField: The Pydantic ModelField object that describes the field.
-            Any: The current value of the field.
-
-    Returns:
-        A dictionary containing the processed user configuration fields of the instance.
-    """
-    user_config_values = {}
-
-    for name, value in instance.__dict__.items():
-        field = instance.__fields__[name]
-        if "user_configurable" in field.field_info.extra:
-            user_config_values[name] = get_field_value(field, value)
-        elif isinstance(value, SystemConfiguration):
-            user_config_values[name] = _recurse_user_config_values(
-                instance=value, get_field_value=get_field_value
-            )
-        elif isinstance(value, list) and all(
-            isinstance(i, SystemConfiguration) for i in value
-        ):
-            user_config_values[name] = [
-                _recurse_user_config_values(i, get_field_value) for i in value
-            ]
-        elif isinstance(value, dict) and all(
-            isinstance(i, SystemConfiguration) for i in value.values()
-        ):
-            user_config_values[name] = {
-                k: _recurse_user_config_values(v, get_field_value)
-                for k, v in value.items()
-            }
-
-    return user_config_values
-
-
-def _get_non_default_user_config_values(instance: BaseModel) -> dict[str, Any]:
-    """
-    Get the non-default user config fields of a Pydantic model instance.
-
-    Params:
-        instance: The Pydantic model instance.
-
-    Returns:
-        dict[str, Any]: The non-default user config values on the instance.
-    """
-
-    def get_field_value(field: ModelField, value):
-        default = field.default_factory() if field.default_factory else field.default
-        if value != default:
-            return value
-
-    return remove_none_items(_recurse_user_config_values(instance, get_field_value))
-
-
-def deep_update(original_dict: dict, update_dict: dict) -> dict:
-    """
-    Recursively update a dictionary.
-
-    Params:
-        original_dict (dict): The dictionary to be updated.
-        update_dict (dict): The dictionary to update with.
-
-    Returns:
-        dict: The updated dictionary.
-    """
-    for key, value in update_dict.items():
-        if (
-            key in original_dict
-            and isinstance(original_dict[key], dict)
-            and isinstance(value, dict)
-        ):
-            original_dict[key] = deep_update(original_dict[key], value)
-        else:
-            original_dict[key] = value
-    return original_dict
-
-
-def remove_none_items(d):
-    if isinstance(d, dict):
-        return {
-            k: remove_none_items(v) for k, v in d.items() if v not in (None, Undefined)
-        }
-    return d
+        elif init_sub_config and value is None:
+            sub_config_type =
