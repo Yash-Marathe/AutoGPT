47c1ae930b3e24b3a98ce53250931cabea312554
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:45:42 +0530

    improve

--- benchmark/agbenchmark/challenges/deprecated/code/2_write/artifacts_out/sample_code.py
+++ benchmark/agbenchmark/challenges/deprecated/code/2_write/artifacts_out/sample_code.py
@@ -1,3 +1,9 @@
-def multiply_int(num: int) -> int:
-    multiplied_num = num * 2
-    return multiplied_num
+def multiply_number(number: float) -> float:
+    """
+    This function takes a number (accepts both int and float) as input and returns the result of multiplying it by 2.
+
+    :param number: the number to be multiplied
+    :return: the multiplied number
+    """
+    multiplied_number = number * 2
+    return multiplied_number
