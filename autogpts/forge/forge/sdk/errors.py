4d9dafd2a493c83badd5c0c372a2688dc89c1107
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:45:25 +0530

    improve

--- autogpts/forge/forge/sdk/errors.py
+++ autogpts/forge/forge/sdk/errors.py
@@ -5,43 +5,46 @@ class NotFoundError(Exception):
     pass
 
 
-class AgentException(Exception):
+class AgentError(Exception):
     """Base class for specific exceptions relevant in the execution of Agents"""
 
     message: str
-
     hint: Optional[str] = None
-    """A hint which can be passed to the LLM to reduce reoccurrence of this error"""
 
-    def __init__(self, message: str, *args):
+    def __init__(self, message: str, hint: Optional[str] = None, *args):
         self.message = message
+        self.hint = hint
         super().__init__(message, *args)
 
 
-class ConfigurationError(AgentException):
+class ConfigurationError(AgentError):
     """Error caused by invalid, incompatible or otherwise incorrect configuration"""
 
 
-class InvalidAgentResponseError(AgentException):
+class AgentResponseError(AgentError):
+    """Error caused by issues with the Agent's response format"""
+
+
+class InvalidCommandError(AgentResponseError):
     """The LLM deviated from the prescribed response format"""
 
 
-class UnknownCommandError(AgentException):
+class UnknownCommandError(AgentResponseError):
     """The AI tried to use an unknown command"""
 
     hint = "Do not try to use this command again."
 
 
-class DuplicateOperationError(AgentException):
-    """The proposed operation has already been executed"""
-
-
-class CommandExecutionError(AgentException):
-    """An error occured when trying to execute the command"""
+class CommandExecutionError(AgentError):
+    """Base class for errors caused by issues with command execution"""
 
 
 class InvalidArgumentError(CommandExecutionError):
     """The command received an invalid argument"""
+
+
+class DuplicateOperationError(CommandExecutionError):
+    """The proposed operation has already been executed"""
 
 
 class OperationNotAllowedError(CommandExecutionError):
