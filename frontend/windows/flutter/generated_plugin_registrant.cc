6c31a095645f4f7d31e302d1826911e8c34bbdca
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:45 +0530

    improve

--- frontend/windows/flutter/generated_plugin_registrant.cc
+++ frontend/windows/flutter/generated_plugin_registrant.cc
@@ -2,16 +2,20 @@
 //  Generated file. Do not edit.
 //
 
-// clang-format off
-
-#include "generated_plugin_registrant.h"
-
+// Includes for the plugins to be registered.
 #include <firebase_core/firebase_core_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
+// Include the Flutter plugin registry.
+#include "flutter/flutter_plugin_registry.h"
+
+// Registers the specified plugins with the given plugin registry.
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  // Registers the Firebase Core plugin.
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
+
+  // Registers the Url Launcher plugin for Windows.
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
