5569749fee99a98e4fab6b973e0dc1077482b90a
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:33 +0530

    improve

--- frontend/linux/flutter/generated_plugin_registrant.cc
+++ frontend/linux/flutter/generated_plugin_registrant.cc
@@ -5,11 +5,16 @@
 // clang-format off
 
 #include "generated_plugin_registrant.h"
-
 #include <url_launcher_linux/url_launcher_plugin.h>
 
-void fl_register_plugins(FlPluginRegistry* registry) {
+/*
+ * Register plugins with the FlPluginRegistry.
+ */
+void register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) url_launcher_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
+  
+  // Register url_launcher_linux plugin
   url_launcher_plugin_register_with_registrar(url_launcher_linux_registrar);
 }
+
