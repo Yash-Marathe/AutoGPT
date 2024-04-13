9be6ff5314aa97bf1bc7106b8b5659da9e788742
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:45:59 +0530

    improve

--- benchmark/agbenchmark/challenges/deprecated/code/d2.2_vague/artifacts_in/sample_code.py
+++ benchmark/agbenchmark/challenges/deprecated/code/d2.2_vague/artifacts_in/sample_code.py
@@ -1,10 +1,8 @@
 from typing import List, Optional
 
-
-def two_sum(nums: List, target: int) -> Optional[List[int]]:
+def two_sum(nums: List[int], target: int) -> Optional[List[int]]:
     seen = {}
     for i, num in enumerate(nums):
-        typo
         complement = target - num
         if complement in seen:
             return [seen[complement], i]
