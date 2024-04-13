031428a6472e6263c6e329378090172fe724274e
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:46:37 +0530

    improve

--- autogpts/autogpt/autogpt/core/agent/base.py
+++ autogpts/autogpt/autogpt/core/agent/base.py
@@ -1,26 +1,45 @@
 import abc
 import logging
+import asyncio
+from typing import Any
 from pathlib import Path
 
-
-class Agent(abc.ABC):
+class AbstractAgent(abc.ABC):
     @abc.abstractmethod
     def __init__(self, *args, **kwargs):
         ...
 
     @classmethod
     @abc.abstractmethod
-    def from_workspace(
+    async def from_workspace(
+        cls,
+        workspace_path: Path,
+        logger: logging.Logger,
+    ) -> "AbstractAgent":
+        ...
+
+    @abc.abstractmethod
+    async def determine_next_ability(self, *args, **kwargs) -> Any:
+        ...
+
+    @abc.abstractmethod
+    def __repr__(self) -> str:
+        ...
+
+
+class Agent(AbstractAgent):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        ...
+
+    @classmethod
+    async def from_workspace(
         cls,
         workspace_path: Path,
         logger: logging.Logger,
     ) -> "Agent":
         ...
 
-    @abc.abstractmethod
-    async def determine_next_ability(self, *args, **kwargs):
+    async def determine_next_ability(self, *args, **kwargs) -> Any:
         ...
 
-    @abc.abstractmethod
-    def __repr__(self):
-        ...
