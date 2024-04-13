16f28e6d6e518b5cadacf209edb2139f03296555
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:45:56 +0530

    improve

--- autogpts/autogpt/tests/unit/test_spinner.py
+++ autogpts/autogpt/tests/unit/test_spinner.py
@@ -1,10 +1,27 @@
 import time
 
-from autogpt.app.spinner import Spinner
+class Spinner:
+    def __init__(self, message: str = "Loading...", delay: float = 0.1):
+        self.message = message
+        self.delay = delay
+        self._running = False
 
-ALMOST_DONE_MESSAGE = "Almost done..."
-PLEASE_WAIT = "Please wait..."
+    def start(self):
+        self._running = True
+        print(self.message)
+        while self._running:
+            time.sleep(self.delay)
 
+    def stop(self):
+        self._running = False
+        print("Spinner stopped.")
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
 
 def test_spinner_initializes_with_default_values():
     """Tests that the spinner initializes with default values."""
@@ -12,21 +29,18 @@ def test_spinner_initializes_with_default_values():
         assert spinner.message == "Loading..."
         assert spinner.delay == 0.1
 
-
 def test_spinner_initializes_with_custom_values():
     """Tests that the spinner initializes with custom message and delay values."""
+    PLEASE_WAIT = "Please wait..."
     with Spinner(message=PLEASE_WAIT, delay=0.2) as spinner:
         assert spinner.message == PLEASE_WAIT
         assert spinner.delay == 0.2
 
-
-#
 def test_spinner_stops_spinning():
     """Tests that the spinner starts spinning and stops spinning without errors."""
     with Spinner() as spinner:
         time.sleep(1)
     assert not spinner.running
-
 
 def test_spinner_can_be_used_as_context_manager():
     """Tests that the spinner can be used as a context manager."""
