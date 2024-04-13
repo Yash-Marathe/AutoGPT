9eb698c4beb8e38936ab9231080a798af3c2e56d
Yash Marathe <109474841+Yash-Marathe@users.noreply.github.com>
2024-04-13 08:48:08 +0530

    improve

--- frontend/lib/utils/uri_utility.dart
+++ frontend/lib/utils/uri_utility.dart
@@ -2,20 +2,17 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class UriUtility {
-  static bool isURL(String url) {
-    // Validate if the URL string is empty, or contains spaces or invalid characters
+  static bool isValidUrl(String url) {
     if (url.isEmpty || RegExp(r'[\s<>]').hasMatch(url)) {
       print('URL is either empty or contains spaces/invalid characters.');
       return false;
     }
 
-    // Check for 'mailto:' at the start of the URL
     if (url.startsWith('mailto:')) {
       print('URL starts with "mailto:".');
       return false;
     }
 
-    // Try to parse the URL, return false if parsing fails
     Uri? uri;
     try {
       uri = Uri.parse(url);
@@ -24,13 +21,11 @@ class UriUtility {
       return false;
     }
 
-    // Validate the URL has a scheme (protocol) and host
     if (uri.scheme.isEmpty || uri.host.isEmpty) {
       print('URL is missing a scheme (protocol) or host.');
       return false;
     }
 
-    // Check if the URI has user info, which is not a common case for a valid HTTP/HTTPS URL
     if (uri.hasAuthority &&
         uri.userInfo.contains(':') &&
         uri.userInfo.split(':').length > 2) {
@@ -38,7 +33,6 @@ class UriUtility {
       return false;
     }
 
-    // Validate the port number if exists
     if (uri.hasPort && (uri.port <= 0 || uri.port > 65535)) {
       print('URL contains an invalid port number.');
       return false;
@@ -49,15 +43,13 @@ class UriUtility {
   }
 
   Future<bool> isValidGitHubRepo(String repoUrl) async {
+    if (!isValidUrl(repoUrl)) return false;
+
     var uri = Uri.parse(repoUrl);
-    if (uri.host != 'github.com') {
-      return false;
-    }
+    if (uri.host != 'github.com') return false;
 
     var segments = uri.pathSegments;
-    if (segments.length < 2) {
-      return false;
-    }
+    if (segments.length < 2) return false;
 
     var user = segments[0];
     var repo = segments[1];
@@ -65,9 +57,7 @@ class UriUtility {
     var apiUri = Uri.https('api.github.com', '/repos/$user/$repo');
 
     var response = await http.get(apiUri);
-    if (response.statusCode != 200) {
-      return false;
-    }
+    if (response.statusCode != 200) return false;
 
     var data = json.decode(response.body);
     return data is Map && data['full_name'] == '$user/$repo';
